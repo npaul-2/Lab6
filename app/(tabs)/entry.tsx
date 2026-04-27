474c07de8ee2377feeddf06bf73b6a60456d95b1
Nebo Paul <nebopaul@Mac.lan>
2026-04-26 23:37:19 -0700

    added student data template

--- app/(tabs)/entry.tsx
+++ app/(tabs)/entry.tsx
@@ -55,6 +55,13 @@ export default function Entry() {
       />
 
       <TextInput 
+        placeholder="Age" 
+        keyboardType="numeric"
+        style={styles.input} 
+        onChangeText={(txt) => setFormData({...formData, age: txt})}
+      />
+
+      <TextInput 
         placeholder="GPA (0.0 - 4.0)" 
         keyboardType="numeric"
         style={styles.input} 
@@ -64,14 +71,28 @@ export default function Entry() {
       <TextInput 
         placeholder="Major" 
         style={styles.input} 
-        onChangeText={(txt) => setFormData({...formData, name: txt})}
+        onChangeText={(txt) => setFormData({...formData, major: txt})}
       />
 
       <TextInput 
         placeholder="Units" 
         keyboardType="numeric"
         style={styles.input} 
-        onChangeText={(txt) => setFormData({...formData, gpa: txt})}
+        onChangeText={(txt) => setFormData({...formData, units: txt})}
+      />
+
+      <TextInput 
+        placeholder="Graduation year" 
+        keyboardType="numeric"
+        style={styles.input} 
+        onChangeText={(txt) => setFormData({...formData, gradYear: txt})}
+      />
+
+      <TextInput 
+        placeholder="Unpaid dues" 
+        keyboardType="numeric"
+        style={styles.input} 
+        onChangeText={(txt) => setFormData({...formData, unpaidDues: txt})}
       />
 
       <Button title="Add Student" onPress={handleAddStudent} />
@@ -80,7 +101,7 @@ export default function Entry() {
 }
 
 const styles = StyleSheet.create({
-  container: { padding: 20 },
+  container: { padding: 20, backgroundColor: "gray" },
   title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
   input: { borderBottomWidth: 1, marginBottom: 15, padding: 8, fontSize: 16 }
 });
